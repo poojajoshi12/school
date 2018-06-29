a5dab596caa86109f67bd2f9eb5660b74f6e8def
Pooja <pooja@gmail.com>
2018-06-29 15:31:43 +0530

    First commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Picker, View } from 'react-native';
+
+export default class App extends Component {
+  state = {
+    hand: 'right',
+  }
+
+  render() {
+    return (
+      <View>
+        <Picker>
+          selectedValue={this.state.hand}
+          onValueChange={hand => this.setState({ hand })}
+          style={{ width: 160 }}
+          mode="dropdown">
+          <Picker.Item label="Right Hand" value="right" />
+          <Picker.Item label="Left Hand" value="left" />
+        </Picker>
+      </View>
+    );
+  }
+}
